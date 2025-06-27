a125ef14acb767a9496b056e21dee9cc9b4d5723
Alexey V. Rybakin <ryb_av@mail.ru>
2025-06-27 10:36:07 +0500

    add EchoHandler implementation

---
+++ src/echo_handler.cpp
@@ -0,0 +1,9 @@
+#include "../include/echo_handler.hpp"
+#include "request_handler.hpp"
+
+RequestHandlerString* EchoHandler::handle_request(const RequestHandlerString* request) {
+    const char* req_str = RequestHandler::get_cstring(request);
+    char* response = new char[strlen(req_str) + 7];
+    sprintf(response, "ECHO: %s", req_str);
+    return RequestHandler::create_string(response);
+}
